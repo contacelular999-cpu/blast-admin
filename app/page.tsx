f0ed0a03eec3d22c2a16651f4bc6c416ab7f2c57
contacelular999-cpu <contacelular999@gmail.com>
2025-12-27 21:48:58 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Blast Admin</h1>
+      <p>Painel administrativo funcionando.</p>
+
+      <a href="/dashboard">Ir para o Dashboard</a>
+    </main>
+  );
+}
